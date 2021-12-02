dd9ee97ee6469222f110774bb2bda2296bd27c42
Philipp Krauss <philipp.krauss@codecentric.de>
2021-12-02 10:30:32 +0100

    solve day 2 part 2

--- 2021/02/02.groovy
+++ 2021/02/02.groovy
@@ -16,4 +16,27 @@ new File('input.txt').eachLine { String line ->
 			break
 	}
 }
-println depth * horizontal
+println 'part 1: ' +  depth * horizontal
+
+depth = 0
+horizontal = 0
+int aim = 0
+
+new File('input.txt').eachLine { String line ->
+	String command = line.split()[0]
+	int value = Integer.parseInt(line.split()[1])
+	switch (command) {
+		case 'forward':
+			horizontal += value
+			depth += aim * value
+			break
+		case 'down':
+			aim += value
+			break;
+		case 'up':
+			aim -= value
+			break
+	}
+}
+
+println 'part 2: ' +  depth * horizontal
