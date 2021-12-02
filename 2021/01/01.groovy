f1991fc5346b7f91e226b96b28f0b7a52dac6e2a
Philipp Krauss <philipp.krauss@codecentric.de>
2021-12-02 10:09:10 +0100

    solve day 1 part 2

--- 2021/01/01.groovy
+++ 2021/01/01.groovy
@@ -15,3 +15,22 @@ static int readFirstValue() {
 	new File("input.txt").withReader { firstValue = Integer.parseInt(it.readLine()) }
 	firstValue
 }
+
+Queue<Integer> queue = new ArrayDeque<Integer>()
+int currentSum = 0
+increases = 0
+new File('input.txt').eachLine { String line ->
+	int current = Integer.parseInt(line)
+	queue << current
+	if (queue.size() > 3) {
+		queue.poll()
+	}
+	if (queue.size() == 3) {
+		if (queue.value.sum() > currentSum && currentSum != 0) {
+			increases++
+		}
+		currentSum = queue.value.sum()
+	}
+}
+
+println 'number of summed increases: ' + increases
